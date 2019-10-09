989c5038ab9a9c1aa54a84e51faceb8cf20ac5c6
Rinoloriy <gtxfrx@gmail.com>
2019-10-09 19:36:05 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,13 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 let kickMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if(!kickMember) return message.channel.send("Пользователь не найден");
+if(!kickMember) return message.channel.send("Пользователь не найден").then(msg => msg.delete(2000)) ;
 
 let reason = args.slice(1).join(" ");
 if(!reason) reason = "Нету причины!";
 
 kickMember.kick();
-
+  
+message.delete();
+  
 message.channel.send(`Произошёл кик... Помянем.`).then(msg => msg.delete(2000));
 
 }
