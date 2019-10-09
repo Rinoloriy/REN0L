f732dc0433b8e80188c4bd6e52b9cf83cf4d0552
Rinoloriy <gtxfrx@gmail.com>
2019-10-09 03:24:42 +0300

    Update cl.js

--- commands/cl.js
+++ commands/cl.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if (isNaN(args[0])) return message.channel.send('`ВВЕДИТЕ ЧИСЛО`');
+  if (args[0] > 100) return message.channel.send('`ВВЕДИТЕ ЧИСЛО МЕНЬШЕ 100`');
   if(!args[0]) return message.channel.send("`ВВЕДИТЕ ЧИСЛО`");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Удалено ${args[0]} сообщений.`).then(msg => msg.delete(2000));
