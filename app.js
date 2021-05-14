7747d8a79812593590dc3bd33c0fadffdcb9a6c1
Rinoloriy <gtxfrx@gmail.com>
2021-05-14 21:13:21 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({partials:["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 
-const prefix = "~";
+const prefix = '~';
 
 const fs = require('fs');
 
@@ -10,27 +10,26 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+    const command = require(`./commands/${file}`);
 
-	client.commands.set(command.name, command);
+    client.commands.set(command.name, command);
 }
 
+
 client.on('ready', () => {
-	console.log('БОТ ЗАПУЩЕН, ПРЕФИКС "~"');
+    console.log('вкл');
 });
+
+
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-	if (command === 'reactionrole') {
-		client.commands.get('reactionrole').execute(message, args);
-	} else if (command === 'cl') {
-		client.commands.get('cl').execute(message, args);
-	} else if (command === 'stt') {
-		client.commands.get('stt').execute(message, args);
-	} else if (command === 'serverinfo') {
-		client.commands.get('serverinfo').execute(client);
-	}
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'reactionrole') {
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
+    }
 
 });
 
