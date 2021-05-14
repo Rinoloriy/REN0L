3c586e9ae6b9e243719afb1d74a2cbb476575e25
Rinoloriy <gtxfrx@gmail.com>
2021-05-14 18:32:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ client.on('ready', () => {
 });
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-  const args = message.content.slice(prefix.length).split(/ +/));
+  const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 	if (command === 'reactionrole') {
 		client.commands.get('reactionrole').execute(Discord, client, args, message);
