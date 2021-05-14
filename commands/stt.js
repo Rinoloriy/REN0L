f90339a77cb2753a7d40b138ed850f0827a31f7b
Rinoloriy <gtxfrx@gmail.com>
2021-05-14 18:34:27 +0300

    Update stt.js

--- commands/stt.js
+++ commands/stt.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'stt',
   description:"отправляет говно",
 
-execute(client, message, args) {
+execute async(client, message, args) {
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
