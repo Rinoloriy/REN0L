2c62de1c068e67b85dfb0015fafb867760c566ed
Rinoloriy <gtxfrx@gmail.com>
2021-05-14 21:16:58 +0300

    Add files via upload

---
+++ reactionrole.js
@@ -0,0 +1,61 @@
+module.exports = {
+    name: 'reactionrole',
+    description: "Роль за реакцию",
+    async execute(message, args, Discord, client) {
+        const channel = '842409612665159692';
+        const dota2role = message.guild.roles.cache.find(role => role.name === "dota2");
+        const csgorole = message.guild.roles.cache.find(role => role.name === "csgo");
+
+        const dota2em = '842771650880864256';
+        const csgoem = '842771650880864256';
+
+        let embed = new Discord.MessageEmbed()
+            .setColor('#e42643')
+            .setTitle('Выбери роль!')
+            .setDescription('Нажми на эмоцию и получи роль!');
+
+        let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(yellowTeamEmoji);
+        messageEmbed.react(blueTeamEmoji);
+
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === dota2em) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(dota2role);
+                }
+                if (reaction.emoji.name === csgoem) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(csgorole);
+                }
+            } else {
+                return;
+            }
+
+        });
+
+        client.on('messageReactionRemove', async (reaction, user) => {
+
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === yellowTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(yellowTeamRole);
+                }
+                if (reaction.emoji.name === blueTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(blueTeamRole);
+                }
+            } else {
+                return;
+            }
+        });
+    }
+
+}
